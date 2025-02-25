985cdfaddc4e7270328c0d9f0661c93c9e5f04c4
usuario <JesusCMM>
2025-02-25 08:06:13 +0000

    primer script SSOO

---
+++ Administración SSOO/Scripts1-4/Apartado1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+	echo "No has introducido exactamente tres argumentos. Vuelve a intentarlo" 
+	exit
+fi
+num1=$1
+op=$2
+num2=$3
+
+if [[ ! $1 =~ ^[0-9]+$ ]]; then
+	echo "El primer parámetro no es un número"
+	exit
+fi
+
+if [ "$op" != "+" ] && [ "$op" != "-" ] && [ "$op" != "x" ] && [ "$op" != "/" ]; then
+        echo "El operador no es válido, por favor introduce + - x /"
+        exit
+fi
+
+if [[ ! $3 =~ ^[0-9]+$ ]]; then
+        echo "El segundo parámetro no es un número"
+        exit
+fi
+
+case "$op" in
+    "+")
+        echo "La suma de $num1 y $num2 es $(( num1 + num2 ))"
+        ;;
+    "-")
+        echo "La resta de $num1 y $num2 es $(( num1 - num2 ))"
+        ;;
+    "x")
+        echo "La multiplicación de $num1 y $num2 es $(( num1 * num2 ))"
+        ;;
+    "/")
+        if [ "$num2" -eq 0 ]; then
+            echo "No se puede dividir entre cero."
+            exit 1
+        fi
+        echo "La división de $num1 y $num2 es $(( num1 / num2 ))"
+        ;;
+    *)
+        echo "Operador no reconocido."
+        exit 1
+        ;;
+esac
---
+++ Administración SSOO/Scripts1-4/Apartado2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+read -p "Introduce un directorio: " dir
+
+if [ ! -d "$dir" ]; then
+    echo "El directorio '$dir' no existe."
+    exit
+fi
+
+#Hasta aquí hemos verificado si el directorio existe
+
+read -p "Introduce el nombre del fichero en del directorio: " fich
+ruta="$dir/$fich"
+
+#Vamos a pedir los cinco nombres con un bucle for dentro del if que comprueba si el fichero existe
+#El pipe hacia awk nos permite imprimir la primera columna, que tiene los permisos
+
+if [ -e "$ruta" ]; then
+    echo "El fichero '$fich' existe."
+    echo "Permisos del fichero:"
+    ls -l "$ruta" | awk '{print $1}'
+ 	for i in {1..5}; do
+        read -p "Introduce el nombre completo del usuario $i: " nombre
+        echo "$i:$nombre" >> "$ruta"
+    	done
+    	echo "Nombres almacenados en '$ruta'."
+else
+    echo "El fichero '$fich' no existe."
+    exit 1
+fi
