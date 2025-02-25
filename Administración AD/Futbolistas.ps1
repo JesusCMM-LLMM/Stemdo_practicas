e273af12bc4179ab253eb76cb1c1f3bf48521d7e
JesusCMM <jesuscmoramesa@gmail.com>
2025-02-25 09:24:32 +0100

    Update Futbolistas.ps1

--- Administración AD/Futbolistas.ps1
+++ Administración AD/Futbolistas.ps1
@@ -1,3 +1,11 @@
+# Definir la ruta de la Unidad Organizativa (OU)
+$ouPath = "OU=Jugadores,DC=miequipo,DC=local"
+
+# Verificar si la OU ya existe antes de crearla (me ha ayudado vicky)
+     if (-not (Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$ouPath'" -ErrorAction SilentlyContinue)) {
+     New-ADOrganizationalUnit -Name "Jugadores" -Path "DC=miequipo,DC=local"
+     }
+
 Import-Csv -Path C:\futbolistas.csv | ForEach-Object {
      # Parámetros
      $nombreCompleto = "$($_.FirstName) $($_.LastName)"
@@ -10,10 +18,7 @@ Import-Csv -Path C:\futbolistas.csv | ForEach-Object {
      if (-not (Get-ADGroup -Filter { Name -eq $grupo } -ErrorAction SilentlyContinue)) {
          New-ADGroup -Name $grupo -GroupScope Global -Path "OU=Jugadores,DC=miequipo,DC=local"
          }
-     # Verificar si la OU ya existe antes de crearla (me ha ayudado vicky)
-     if (-not (Get-ADOrganizationalUnit -Filter "DistinguishedName -eq '$ouPath'" -ErrorAction SilentlyContinue)) {
-     New-ADOrganizationalUnit -Name "Jugadores" -Path "DC=miequipo,DC=local"
-     }
+    
 
      # Crea usuarios
      New-ADUser -Name $nombreCompleto `
