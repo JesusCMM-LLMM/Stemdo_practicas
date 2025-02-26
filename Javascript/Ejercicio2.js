406a972a47f51c6c883207986af1c67bf9105372
JesusCMM <jesuscmoramesa@gmail.com>
2025-02-26 14:58:11 +0100

    Add files via upload

---
+++ Javascript/Ejercicio2.js
@@ -0,0 +1,18 @@
+// Obtener el nombre del script
+const scriptName = process.argv[1].split('/').pop();
+
+// Obtener los parámetros que se le pasan al script
+const params = process.argv.slice(2); //Como el argumento 1 y 2 son respectivamente la ruta al node js y la ruta al ejecutable, los cortamos con el slice
+const numberOfParams = params.length;
+
+// Obtener información del usuario
+const username = process.env.USERNAME; //usuario
+const homeDir = process.env.HOMEPATH; //ruta al dir home
+const currentDir = process.cwd(); // dir actual
+
+// Mostrar la información
+console.log(`Nombre del script: ${scriptName}`);
+console.log(`Parámetros recibidos: ${params.length}`);
+console.log(`Nombre de usuario: ${username}`);
+console.log(`Directorio home del usuario: ${homeDir}`);
+console.log(`Directorio actual: ${currentDir}`);
