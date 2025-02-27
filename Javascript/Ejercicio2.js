f32987633781ff6f0d03c9c9a2f0fcc6e0bed50f
JesusCMM <jesuscmoramesa@gmail.com>
2025-02-27 10:02:59 +0100

    Update Ejercicio2.js

--- Javascript/Ejercicio2.js
+++ Javascript/Ejercicio2.js
@@ -1,18 +1,18 @@
 // Obtener el nombre del script
-const scriptName = process.argv[1].split('/').pop(); //pop nos coge el ultimo elemento de la cadena, / es el separador y argv[1] es la ruta del script
+const NomScript = process.argv[1].split('/').pop();
 
 // Obtener los parámetros que se le pasan al script
 const params = process.argv.slice(2); //Como el argumento 1 y 2 son respectivamente la ruta al node js y la ruta al ejecutable, los cortamos con el slice
-const numberOfParams = params.length;
+const NumParams = params.length;
 
 // Obtener información del usuario
-const username = process.env.USERNAME; //usuario
-const homeDir = process.env.HOMEPATH; //ruta al dir home
-const currentDir = process.cwd(); // dir actual
+const usuario = process.env.USERNAME; 
+const DirHome = process.env.HOMEPATH; //ruta al dir home
+const DirActual = process.cwd(); // dir actual
 
 // Mostrar la información
-console.log(`Nombre del script: ${scriptName}`);
+console.log(`Nombre del script: ${NomScript}`);
 console.log(`Parámetros recibidos: ${params.length}`);
-console.log(`Nombre de usuario: ${username}`);
-console.log(`Directorio home del usuario: ${homeDir}`);
-console.log(`Directorio actual: ${currentDir}`);
+console.log(`Nombre de usuario: ${usuario}`);
+console.log(`Directorio home del usuario: ${DirHome}`);
+console.log(`Directorio actual: ${DirActual}`);
