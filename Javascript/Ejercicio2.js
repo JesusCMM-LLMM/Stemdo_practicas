83a83d473ba621c75d75d46224f089fa71c1d277
JesusCMM <jesuscmoramesa@gmail.com>
2025-02-27 10:10:01 +0100

    Update Ejercicio2.js

--- Javascript/Ejercicio2.js
+++ Javascript/Ejercicio2.js
@@ -3,7 +3,6 @@ const NomScript = process.argv[1].split('/').pop();
 
 // Obtener los parámetros que se le pasan al script
 const params = process.argv.slice(2); //Como el argumento 1 y 2 son respectivamente la ruta al node js y la ruta al ejecutable, los cortamos con el slice
-const NumParams = params.length;
 
 // Obtener información del usuario
 const usuario = process.env.USERNAME; 
